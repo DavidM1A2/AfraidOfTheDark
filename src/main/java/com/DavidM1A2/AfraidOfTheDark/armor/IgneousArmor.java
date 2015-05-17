f68d9c4f72ab4c6c0d9544fc7de86ea19d421267
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-05-17 00:22:24 -0700

    Igneous Armor effects

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/armor/IgneousArmor.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/armor/IgneousArmor.java
@@ -1,7 +1,9 @@
 package com.DavidM1A2.AfraidOfTheDark.armor;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 import com.DavidM1A2.AfraidOfTheDark.initializeMod.ModItems;
 import com.DavidM1A2.AfraidOfTheDark.refrence.Refrence;
@@ -13,6 +15,23 @@ public class IgneousArmor extends AOTDArmor
 		super(armorMaterial, renderIndex, type);
 		this.setCreativeTab(Refrence.AFRAID_OF_THE_DARK);
 		this.setUnlocalizedName((type == 0) ? "igneousHelmet" : (type == 1) ? "igneousChestplate" : (type == 2) ? "igneousLeggings" : "igneousBoots");
+	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
+	{
+		super.onArmorTick(world, player, itemStack);
+		if (player.inventory.armorInventory[0] != null && player.inventory.armorInventory[1] != null && player.inventory.armorInventory[2] != null && player.inventory.armorInventory[3] != null)
+		{
+			if (player.inventory.armorInventory[0].getItem() instanceof IgneousArmor && player.inventory.armorInventory[1].getItem() instanceof IgneousArmor && player.inventory.armorInventory[2].getItem() instanceof IgneousArmor
+					&& player.inventory.armorInventory[3].getItem() instanceof IgneousArmor)
+			{
+				if (player.isBurning())
+				{
+					player.extinguish();
+				}
+			}
+		}
 	}
 
 	@Override
