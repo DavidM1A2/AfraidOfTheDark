7b11c46b4a05c265097b8b6da9d6732f05b38e1b
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-06-04 18:32:44 -0700

    Fixed a crash. Herp derp

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/packets/UpdateVitae.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/packets/UpdateVitae.java
@@ -5,42 +5,41 @@
  */
 package com.DavidM1A2.AfraidOfTheDark.packets;
 
-import com.DavidM1A2.AfraidOfTheDark.playerData.HasStartedAOTD;
-import com.DavidM1A2.AfraidOfTheDark.playerData.Vitae;
-import com.DavidM1A2.AfraidOfTheDark.utility.LogHelper;
-
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
+import com.DavidM1A2.AfraidOfTheDark.playerData.Vitae;
+import com.DavidM1A2.AfraidOfTheDark.utility.LogHelper;
+
 public class UpdateVitae implements IMessage
 {
 	private int vitaeLevel = 0;
 	private int entityIDToUpdate = 0;
-	
+
 	public UpdateVitae()
 	{
 		this.vitaeLevel = 0;
 		this.entityIDToUpdate = 0;
 	}
-	
+
 	public UpdateVitae(int vitaeLevel, int entityIDToUpdate)
 	{
 		this.vitaeLevel = vitaeLevel;
 		this.entityIDToUpdate = entityIDToUpdate;
 	}
-	
+
 	@Override
-	public void fromBytes(ByteBuf buf) 
+	public void fromBytes(ByteBuf buf)
 	{
 		this.vitaeLevel = buf.readInt();
 		this.entityIDToUpdate = buf.readInt();
 	}
 
 	@Override
-	public void toBytes(ByteBuf buf) 
+	public void toBytes(ByteBuf buf)
 	{
 		buf.writeInt(this.vitaeLevel);
 		buf.writeInt(this.entityIDToUpdate);
@@ -65,7 +64,7 @@ public class UpdateVitae implements IMessage
 		public IMessage onMessage(final UpdateVitae message, final MessageContext ctx)
 		{
 			LogHelper.info("Update Vitae Status: " + message.vitaeLevel + " on entity " + Minecraft.getMinecraft().thePlayer.worldObj.getEntityByID(message.entityIDToUpdate).getName());
-			Minecraft.getMinecraft().thePlayer.worldObj.getEntityByID(message.entityIDToUpdate).getEntityData().setInteger(Vitae.VITAE_LEVEL, message.vitaeLevel);
+			Minecraft.getMinecraft().thePlayer.getEntityData().setInteger(Vitae.VITAE_LEVEL, message.vitaeLevel);
 			return null;
 		}
 	}
