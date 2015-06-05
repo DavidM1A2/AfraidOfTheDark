6864c9d96644ed097495aae7a39c928eba9cd8f6
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-06-04 23:06:15 -0700

    Springs generate

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/initializeMod/ModGeneration.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/initializeMod/ModGeneration.java
@@ -7,19 +7,19 @@ package com.DavidM1A2.AfraidOfTheDark.initializeMod;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-import com.DavidM1A2.AfraidOfTheDark.worldGeneration.GenerateSilverOre;
+import com.DavidM1A2.AfraidOfTheDark.worldGeneration.GenerateSprings;
 import com.DavidM1A2.AfraidOfTheDark.worldGeneration.VeronaTreeV40withleaves;
 
 public class ModGeneration
 {
 	// Declare ore generators
-	public static final GenerateSilverOre generateSilverOre = new GenerateSilverOre();
 	public static final VeronaTreeV40withleaves generateTree = new VeronaTreeV40withleaves();
+	public static final GenerateSprings generateSprings = new GenerateSprings();
 
 	public static void initialize()
 	{
 		// Register generators
-		GameRegistry.registerWorldGenerator(ModGeneration.generateSilverOre, 1);
+		GameRegistry.registerWorldGenerator(ModGeneration.generateSprings, 1);
 		// GameRegistry.registerWorldGenerator(generateTree, 1);
 	}
 }
