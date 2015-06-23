350ad0b7a0042e0a56394de68076d363eeea69be
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-06-23 12:01:35 -0700

    Work on journal research gui

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/BloodStainedJournalPageGUI.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/BloodStainedJournalPageGUI.java
@@ -6,6 +6,8 @@
 package com.DavidM1A2.AfraidOfTheDark.client.gui;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -25,9 +27,10 @@ import com.DavidM1A2.AfraidOfTheDark.common.refrence.ClientData;
 
 public class BloodStainedJournalPageGUI extends GuiScreen
 {
-	private String textPrevious;
-	private String textCurrent;
-	private String textNext;
+	private final String originalCompleteText;
+
+	private final List<String> textOnEachPage;
+
 	private final String title;
 
 	private final TextBox leftPage;
@@ -57,17 +60,25 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 	{
 		// Setup tile and page text. Then add left and right page text boxes
 		super();
-		this.textNext = textNext;
+		this.originalCompleteText = textNext;
 		this.title = title;
+		this.textOnEachPage = new LinkedList<String>();
+
 		this.leftPage = new TextBox(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalWidth, ClientData.journalFont);
 		this.rightPage = new TextBox(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalWidth, ClientData.journalFont);
-		leftPageLabel = new PageNumberLabel(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalHeight, ClientData.journalFont);
-		rightPageLabel = new PageNumberLabel(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalHeight, ClientData.journalFont);
+		this.leftPageLabel = new PageNumberLabel(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalHeight, ClientData.journalFont);
+		this.rightPageLabel = new PageNumberLabel(this.xCornerOfPage, this.yCornerOfPage, this.journalWidth, this.journalHeight, ClientData.journalFont);
 		this.forwardButton = new ForwardBackwardButtons(15, this.width - 64, this.height - 64, 64, 64, true);
 		this.backwardButton = new ForwardBackwardButtons(16, 0, this.height - 64, 64, 64, false);
-
 		this.bookmarkButton = new BookmarkButton(1, 0, (int) (this.yCornerOfPage + this.journalWidth / 2.1), this.width, 40);
 		this.journalTexture = new ResourceLocation("afraidofthedark:textures/gui/bloodStainedJournalPage.png");
+
+		this.updateBounds();
+
+		//this.textPrevious = "";
+		//this.textCurrent = this.textNext.substring(0, leftPage.getExtraText(rightPage.getExtraText(textNext)).length());
+		//LogHelper.info(leftPage.getExtraText(rightPage.getExtraText(textNext)).length());
+		//this.textNext = this.textNext.substring(textCurrent.length(), this.textNext.length());
 	}
 
 	@Override
@@ -78,10 +89,6 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 		this.buttonList.add(this.bookmarkButton);
 		this.buttonList.add(this.forwardButton);
 		this.buttonList.add(this.backwardButton);
-
-		this.textPrevious = "";
-		this.textCurrent = this.textNext.substring(0, this.textNext.length() - leftPage.getExtraText(rightPage.getExtraText(textNext)).length());
-		this.textNext = leftPage.getExtraText(rightPage.getExtraText(textNext));
 	}
 
 	// Opening a research book DOES NOT pause the game (unlike escape)
@@ -103,37 +110,8 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 		// Has the window been resized?
 		if ((this.width != this.previousWidth) || (this.height != this.previousHeight))
 		{
-			// Calculate various variables later used in text box width/height calculation
-			this.xCornerOfPage = (this.width - 330) / 2;
-			this.yCornerOfPage = (this.height - 330) / 2;
-
-			this.journalWidth = 330;
-			this.journalHeight = 330;
-
-			// Set the journal font sizes
-			ClientData.journalFont.setFontSize(20, 32, 126, false);
-			ClientData.journalTitleFont.setFontSize(32, 32, 126, false);
-
-			int scaledXLeftPageCoord = this.xCornerOfPage + 20;
-			int scaledYLeftPageCoord = this.yCornerOfPage + 35;
-
-			int scaledXRightPageCoord = this.xCornerOfPage + 170;
-			int scaledYRightPageCoord = this.yCornerOfPage + 35;
-
-			// Set the text box bounds
-			this.leftPage.updateBounds(scaledXLeftPageCoord, scaledYLeftPageCoord, this.journalWidth, this.journalHeight - 20);
-			this.rightPage.updateBounds(scaledXRightPageCoord, scaledYRightPageCoord, this.journalWidth, this.journalHeight - 20);
-
-			this.leftPageLabel.updateBounds(scaledXLeftPageCoord, scaledYLeftPageCoord, 50, 50);
-			this.rightPageLabel.updateBounds(scaledXRightPageCoord, scaledYRightPageCoord, 50, 50);
-
-			this.forwardButton.updateBounds(this.width - 64, this.height - 64);
-			this.backwardButton.updateBounds(0, this.height - 64);
-
-			this.bookmarkButton.updateBounds(64, this.height - 40, this.width - 128, 40);
-
-			this.previousWidth = this.width;
-			this.previousHeight = this.height;
+			updateBounds();
+			updateText();
 		}
 
 		GL11.glPushMatrix();
@@ -152,8 +130,23 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 
 		// Draw the title
 		ClientData.journalTitleFont.drawString(this.title, this.xCornerOfPage + 15, this.yCornerOfPage + 15, 0xFF800000);
-		// Anything the left page can't draw, move to right page
-		this.rightPage.drawText(this.leftPage.drawText(this.textCurrent));
+
+		try
+		{
+			this.leftPage.drawText(this.textOnEachPage.get(pageNumber));
+		}
+		catch (IndexOutOfBoundsException e)
+		{
+			this.leftPage.drawText("");
+		}
+		try
+		{
+			this.rightPage.drawText(this.textOnEachPage.get(pageNumber + 1));
+		}
+		catch (IndexOutOfBoundsException e)
+		{
+			this.rightPage.drawText("");
+		}
 
 		this.leftPageLabel.drawNumber(Integer.toString(this.pageNumber + 1));
 		this.rightPageLabel.drawNumber(Integer.toString(this.pageNumber + 2));
@@ -165,6 +158,41 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 
 		this.backwardButton.drawButton(mc, mouseX, mouseY);
 		this.forwardButton.drawButton(mc, mouseX, mouseY);
+	}
+
+	private void updateBounds()
+	{
+		this.journalWidth = 330;
+		this.journalHeight = 330;
+
+		// Calculate various variables later used in text box width/height calculation
+		this.xCornerOfPage = (this.width - journalWidth) / 2;
+		this.yCornerOfPage = (this.height - journalHeight) / 2;
+
+		// Set the journal font sizes
+		ClientData.journalFont.setFontSize(20, 32, 126, false);
+		ClientData.journalTitleFont.setFontSize(32, 32, 126, false);
+
+		int scaledXLeftPageCoord = this.xCornerOfPage + 20;
+		int scaledYLeftPageCoord = this.yCornerOfPage + 35;
+
+		int scaledXRightPageCoord = this.xCornerOfPage + 170;
+		int scaledYRightPageCoord = this.yCornerOfPage + 35;
+
+		// Set the text box bounds
+		this.leftPage.updateBounds(scaledXLeftPageCoord, scaledYLeftPageCoord, this.journalWidth, this.journalHeight - 50);
+		this.rightPage.updateBounds(scaledXRightPageCoord, scaledYRightPageCoord, this.journalWidth, this.journalHeight - 50);
+
+		this.leftPageLabel.updateBounds(scaledXLeftPageCoord, scaledYLeftPageCoord + this.journalHeight - 85, 50, 50);
+		this.rightPageLabel.updateBounds(scaledXRightPageCoord + (int) (this.journalWidth * .42), scaledYRightPageCoord + this.journalHeight - 85, 50, 50);
+
+		this.forwardButton.updateBounds(this.width - 64, this.height - 64);
+		this.backwardButton.updateBounds(0, this.height - 64);
+
+		this.bookmarkButton.updateBounds(64, this.height - 40, this.width - 128, 40);
+
+		this.previousWidth = this.width;
+		this.previousHeight = this.height;
 	}
 
 	@Override
@@ -188,18 +216,42 @@ public class BloodStainedJournalPageGUI extends GuiScreen
 
 	private void advancePage()
 	{
-		this.textPrevious = this.textPrevious + this.textCurrent;
-		this.textCurrent = this.textNext.substring(0, this.textNext.length() - leftPage.getExtraText(rightPage.getExtraText(textNext)).length());
-		this.textNext = leftPage.getExtraText(rightPage.getExtraText(textNext));
 		pageNumber = pageNumber + 2;
 	}
 
 	private void rewindPage()
 	{
-		this.textNext = this.textNext + this.textCurrent;
-		this.textCurrent = this.textPrevious.substring(leftPage.getExtraText(rightPage.getExtraText(textPrevious)).length(), this.textPrevious.length());
-		this.textPrevious = this.textPrevious.substring(0, textPrevious.length() - textCurrent.length());
 		pageNumber = pageNumber - 2;
+		if (pageNumber < 0)
+		{
+			pageNumber = 0;
+		}
+	}
+
+	private void updateText()
+	{
+		this.textOnEachPage.clear();
+		String textToDistribute = this.originalCompleteText;
+		boolean alternater = true;
+
+		while (!textToDistribute.isEmpty())
+		{
+			String leftOver;
+			if (alternater)
+			{
+				leftOver = leftPage.getExtraText(textToDistribute);
+			}
+			else
+			{
+				leftOver = rightPage.getExtraText(textToDistribute);
+			}
+			alternater = !alternater;
+
+			String page = textToDistribute.substring(0, textToDistribute.length() - leftOver.length());
+			textToDistribute = textToDistribute.substring(textToDistribute.length() - leftOver.length());
+
+			this.textOnEachPage.add(page);
+		}
 	}
 
 	// If E is typed we close the GUI screen
