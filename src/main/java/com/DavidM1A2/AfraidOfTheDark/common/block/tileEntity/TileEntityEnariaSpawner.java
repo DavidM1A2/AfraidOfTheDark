ead7d92c12a2b2d141f3669beb399412a417b9eb
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-11-09 16:49:30 -0700

    Enaria spawner fixes

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/common/block/tileEntity/TileEntityEnariaSpawner.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/common/block/tileEntity/TileEntityEnariaSpawner.java
@@ -7,82 +7,92 @@ import com.DavidM1A2.AfraidOfTheDark.common.entities.Enaria.EntityEnaria;
 import com.DavidM1A2.AfraidOfTheDark.common.initializeMod.ModBlocks;
 import com.DavidM1A2.AfraidOfTheDark.common.playerData.AOTDPlayerData;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.EnumDifficulty;
 
 public class TileEntityEnariaSpawner extends AOTDTileEntity
 {
 	private int ticksExisted = 0;
 	private UUID enariaEntityID = null;
-	private static final int TICKS_INBETWEEN_CHECKS = 100;
-	private static final int CHECK_RANGE = 30;
-		
-	public TileEntityEnariaSpawner() 
+	private static final int TICKS_INBETWEEN_CHECKS = 40;
+	private final AxisAlignedBB playerCheckRegion;
+
+	public TileEntityEnariaSpawner()
 	{
 		super(ModBlocks.enariaSpawner);
+		playerCheckRegion = AxisAlignedBB.fromBounds(this.pos.getX() - 11, this.pos.getY() - 2, this.pos.getZ() - 2, this.pos.getX() + 11, this.pos.getY() + 11, this.pos.getZ() + 20);
 	}
-	
+
 	@Override
-	public void update() 
+	public void update()
 	{
 		if (!worldObj.isRemote)
 		{
 			if (ticksExisted % TICKS_INBETWEEN_CHECKS == 0)
 			{
-				if (enariaEntityID == null)
+				if (worldObj.getDifficulty() != EnumDifficulty.PEACEFUL)
 				{
-					for (Object object : this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, AxisAlignedBB.fromBounds(this.pos.getX(), this.pos.getY(), this.pos.getZ(), this.pos.getX() + 1, this.pos.getY() + 1, this.pos.getZ() + 1).expand(CHECK_RANGE, CHECK_RANGE, CHECK_RANGE)))
+					if (enariaEntityID == null)
 					{
-						if (object instanceof EntityPlayer)
+						for (Object object : this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, playerCheckRegion))
 						{
-							EntityPlayer entityPlayer = (EntityPlayer) object;
-							if (!AOTDPlayerData.get(entityPlayer).getHasBeatenEnaria())
+							if (object instanceof EntityPlayer)
 							{
-								this.summonEnaria();
-								return;
+								EntityPlayer entityPlayer = (EntityPlayer) object;
+								if (!AOTDPlayerData.get(entityPlayer).getHasBeatenEnaria())
+								{
+									this.summonEnaria();
+									return;
+								}
 							}
 						}
 					}
-				}
-				else
-				{
-					Entity entity = MinecraftServer.getServer().getEntityFromUuid(enariaEntityID);
-					
-					if (entity == null)
+					else
 					{
-						this.enariaEntityID = null;
+						Entity entity = MinecraftServer.getServer().getEntityFromUuid(enariaEntityID);
+
+						if (entity == null)
+						{
+							this.enariaEntityID = null;
+						}
 					}
 				}
 			}
 			ticksExisted = ticksExisted + 1;
 		}
 	}
-	
+
 	private void summonEnaria()
 	{
 		EntityEnaria enaria = new EntityEnaria(this.worldObj);
 		enaria.getEntityData().setBoolean(EntityEnaria.IS_VALID, true);
-		enaria.setPosition(this.pos.getX(), this.pos.getY() + 10, this.pos.getZ());
+		enaria.setPosition(this.pos.getX() + 0.5, this.pos.getY() + 7, this.pos.getZ() + 0.5);
 		this.worldObj.spawnEntityInWorld(enaria);
 		this.enariaEntityID = enaria.getPersistentID();
 	}
-	
+
 	@Override
-	public void writeToNBT(NBTTagCompound compound) 
+	public void writeToNBT(NBTTagCompound compound)
 	{
-        compound.setLong("enariaEntityIDMost", this.enariaEntityID.getMostSignificantBits());
-        compound.setLong("enariaEntityIDLeast", this.enariaEntityID.getLeastSignificantBits());
+		if (this.enariaEntityID != null)
+		{
+			compound.setLong("enariaEntityIDMost", this.enariaEntityID.getMostSignificantBits());
+			compound.setLong("enariaEntityIDLeast", this.enariaEntityID.getLeastSignificantBits());
+		}
 		super.writeToNBT(compound);
 	}
-	
+
 	@Override
-	public void readFromNBT(NBTTagCompound compound) 
+	public void readFromNBT(NBTTagCompound compound)
 	{
-		this.enariaEntityID = new UUID(compound.getLong("enariaEntityIDMost"), compound.getLong("enariaEntityIDLeast"));
+		if (compound.hasKey("enariaEntityIDMost") && compound.hasKey("enariaEntityIDLeast"))
+		{
+			this.enariaEntityID = new UUID(compound.getLong("enariaEntityIDMost"), compound.getLong("enariaEntityIDLeast"));
+		}
 		super.readFromNBT(compound);
 	}
 }
