5ef4ece43206974af218b317d1bdf49fe108baca
DavidM1A2 <davidslovikosky@email.arizona.edu>
2015-11-18 11:05:52 -0700

    Splinter drone AI improvements

---
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/common/entities/SplinterDrone/EntitySplinterDroneMoveHelper.java
@@ -0,0 +1,71 @@
+/*
+ * Author: David Slovikosky
+ * Mod: Afraid of the Dark
+ * Ideas and Textures: Michael Albertson
+ */
+package com.DavidM1A2.AfraidOfTheDark.common.entities.SplinterDrone;
+
+import net.minecraft.entity.ai.EntityMoveHelper;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MathHelper;
+
+public class EntitySplinterDroneMoveHelper extends EntityMoveHelper
+{
+	private final EntitySplinterDrone splinterDrone;
+	private int temp;
+
+	public EntitySplinterDroneMoveHelper(EntitySplinterDrone splinterDrone)
+	{
+		super(splinterDrone);
+		this.splinterDrone = splinterDrone;
+	}
+
+	@Override
+	public void onUpdateMoveHelper()
+	{
+		if (this.update)
+		{
+			double x = this.posX - this.splinterDrone.posX;
+			double y = this.posY - this.splinterDrone.posY;
+			double z = this.posZ - this.splinterDrone.posZ;
+			double vector = x * x + y * y + z * z;
+
+			if (this.temp-- <= 0)
+			{
+				this.temp += this.splinterDrone.getRNG().nextInt(5) + 2;
+				vector = MathHelper.sqrt_double(vector);
+
+				if (this.canMoveHere(this.posX, this.posY, this.posZ, vector))
+				{
+					this.splinterDrone.motionX = this.splinterDrone.motionX + x / vector * 0.1D;
+					this.splinterDrone.motionY = this.splinterDrone.motionY + y / vector * 0.1D;
+					this.splinterDrone.motionZ = this.splinterDrone.motionZ + z / vector * 0.1D;
+				}
+				else
+				{
+					this.update = false;
+				}
+			}
+		}
+	}
+
+	private boolean canMoveHere(double posX, double posY, double posZ, double vector)
+	{
+		double x = (posX - this.splinterDrone.posX) / vector;
+		double y = (posY - this.splinterDrone.posY) / vector;
+		double z = (posZ - this.splinterDrone.posZ) / vector;
+		AxisAlignedBB axisalignedbb = this.splinterDrone.getEntityBoundingBox();
+
+		for (int i = 1; i < vector; i++)
+		{
+			axisalignedbb = axisalignedbb.offset(x, y, z);
+
+			if (!this.splinterDrone.worldObj.getCollidingBoundingBoxes(this.splinterDrone, axisalignedbb).isEmpty())
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
