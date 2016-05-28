a76c2e0b8998c2fc9713207eeb6daf93b0fb0de5
DavidM1A2 <davidslovikosky@email.arizona.edu>
2016-05-27 18:00:16 -0700

    Slight config changes

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/ModGuiConfig.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/ModGuiConfig.java
@@ -5,6 +5,9 @@
  */
 package com.DavidM1A2.AfraidOfTheDark.client.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.DavidM1A2.AfraidOfTheDark.common.handler.ConfigurationHandler;
 import com.DavidM1A2.AfraidOfTheDark.common.reference.Reference;
 
@@ -12,14 +15,24 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
 
 /*
  * Setup the GUI configuration screen
  */
 public class ModGuiConfig extends GuiConfig
 {
+	private static final List<IConfigElement> CONFIG_INGAME_OPTIONS = new ArrayList<IConfigElement>()
+	{
+		{
+			addAll(new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements());
+			addAll(new ConfigElement(ConfigurationHandler.configuration.getCategory(ConfigurationHandler.CATEGORY_BIOME_IDS)).getChildElements());
+			addAll(new ConfigElement(ConfigurationHandler.configuration.getCategory(ConfigurationHandler.CATEGORY_DUNGEON_FREQUENCY)).getChildElements());
+		}
+	};
+
 	public ModGuiConfig(final GuiScreen guiScreen)
 	{
-		super(guiScreen, new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(), Reference.MOD_ID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+		super(guiScreen, CONFIG_INGAME_OPTIONS, Reference.MOD_ID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
 	}
 }
--- src/main/java/com/DavidM1A2/AfraidOfTheDark/common/handler/ConfigurationHandler.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/common/handler/ConfigurationHandler.java
@@ -32,6 +32,9 @@ public class ConfigurationHandler
 	public static boolean enableAOTDAnimations = true;
 	public static boolean enableWorldGenLightUpdates = false;
 
+	public static final String CATEGORY_BIOME_IDS = "biome ids";
+	public static final String CATEGORY_DUNGEON_FREQUENCY = "dungeon frequency";
+
 	public static void initializataion(final File configFile)
 	{
 		if (ConfigurationHandler.configuration == null)
@@ -54,21 +57,21 @@ public class ConfigurationHandler
 
 	private static void loadConfiguration()
 	{
-		ConfigurationHandler.configuration.addCustomCategoryComment("Biome IDs", "Here you can configure Afraid of the Dark biome IDs");
-		ConfigurationHandler.configuration.addCustomCategoryComment("Dungeon Frequency", "Here you can set how frequently certain dungeons appear.");
-		ConfigurationHandler.biomeErieID = ConfigurationHandler.configuration.getInt("biomeErieID", "Biome IDs", 68, 5, 255, "The Biome ID for the erie forest. Use this to make this mod compatable with other biome mods.");
-		ConfigurationHandler.biomeNightmareID = ConfigurationHandler.configuration.getInt("biomeNightmareID", "Biome IDs", 69, 5, 255, "The Biome ID for the nightmare. Use this to make this mod compatable with other biome mods.");
-		ConfigurationHandler.biomeVoidChestID = ConfigurationHandler.configuration.getInt("biomeVoidChestID", "Biome IDs", 70, 5, 255, "The Biome ID for the Void Chest. Use this to make this mod compatable with other biome mods.");
+		ConfigurationHandler.configuration.addCustomCategoryComment(CATEGORY_BIOME_IDS, "Here you can configure Afraid of the Dark biome IDs");
+		ConfigurationHandler.configuration.addCustomCategoryComment(CATEGORY_DUNGEON_FREQUENCY, "Here you can set how frequently certain dungeons appear.");
+		ConfigurationHandler.biomeErieID = ConfigurationHandler.configuration.getInt("biomeErieID", CATEGORY_BIOME_IDS, 68, 5, 255, "The Biome ID for the erie forest. Use this to make this mod compatable with other biome mods.");
+		ConfigurationHandler.biomeNightmareID = ConfigurationHandler.configuration.getInt("biomeNightmareID", CATEGORY_BIOME_IDS, 69, 5, 255, "The Biome ID for the nightmare. Use this to make this mod compatable with other biome mods.");
+		ConfigurationHandler.biomeVoidChestID = ConfigurationHandler.configuration.getInt("biomeVoidChestID", CATEGORY_BIOME_IDS, 70, 5, 255, "The Biome ID for the Void Chest. Use this to make this mod compatable with other biome mods.");
 		ConfigurationHandler.erieBiomeFrequency = ConfigurationHandler.configuration.getInt("erieBiomeFrequency", Configuration.CATEGORY_GENERAL, 10, 0, 1000, "Increase this value to increase the number of Erie Biomes. 10 is the default MC forest value.");
-		ConfigurationHandler.dungeonFrequencyMultiplier = ConfigurationHandler.configuration.getFloat("dungeonFrequencyMultiplier", "Dungeon Frequency", 1.0f, 0.0f, 1000.0f,
+		ConfigurationHandler.dungeonFrequencyMultiplier = ConfigurationHandler.configuration.getFloat("dungeonFrequencyMultiplier", CATEGORY_DUNGEON_FREQUENCY, 1.0f, 0.0f, 1000.0f,
 				"The dungeon frequency multiplier increases or decreases dungeon rarity across ALL Afraid of the Dark dungeons. (ex. 2.0 would result in double the dungeons and 0.5 in half)");
 
-		ConfigurationHandler.cryptFrequency = ConfigurationHandler.configuration.getFloat("cryptFrequency", "Dungeon Frequency", 0.5f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a crypt will spawn.");
-		ConfigurationHandler.darkForestFrequency = ConfigurationHandler.configuration.getFloat("darkForestFrequency", "Dungeon Frequency", 0.4f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a dark forest will spawn.");
-		ConfigurationHandler.gnomishCityFrequency = ConfigurationHandler.configuration.getFloat("gnomishCityFrequency", "Dungeon Frequency", 0.4f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a gnomish city will spawn.");
-		ConfigurationHandler.springFrequency = ConfigurationHandler.configuration.getFloat("springFrequency", "Dungeon Frequency", 0.32f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a spring will spawn.");
-		ConfigurationHandler.voidChestFrequency = ConfigurationHandler.configuration.getFloat("voidChestFrequency", "Dungeon Frequency", 0.24f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a void chest will spawn.");
-		ConfigurationHandler.witchHutFrequency = ConfigurationHandler.configuration.getFloat("witchHutFrequency", "Dungeon Frequency", 0.8f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a witch hut will spawn.");
+		ConfigurationHandler.cryptFrequency = ConfigurationHandler.configuration.getFloat("cryptFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.5f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a crypt will spawn.");
+		ConfigurationHandler.darkForestFrequency = ConfigurationHandler.configuration.getFloat("darkForestFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.4f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a dark forest will spawn.");
+		ConfigurationHandler.gnomishCityFrequency = ConfigurationHandler.configuration.getFloat("gnomishCityFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.4f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a gnomish city will spawn.");
+		ConfigurationHandler.springFrequency = ConfigurationHandler.configuration.getFloat("springFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.32f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a spring will spawn.");
+		ConfigurationHandler.voidChestFrequency = ConfigurationHandler.configuration.getFloat("voidChestFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.24f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a void chest will spawn.");
+		ConfigurationHandler.witchHutFrequency = ConfigurationHandler.configuration.getFloat("witchHutFrequency", CATEGORY_DUNGEON_FREQUENCY, 0.8f, 0.0f, 100.0f, "This property is the percentage chance per chunk that a witch hut will spawn.");
 
 		ConfigurationHandler.debugMessages = ConfigurationHandler.configuration.getBoolean("debugMessages", Configuration.CATEGORY_GENERAL, false, "If you wish to receive all possible kinds of spammy debug messages in the console turn this on. Mostly used for developers only.");
 
