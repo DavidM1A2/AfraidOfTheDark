5997895475f788229f2a6c943a229675de668550
DavidM1A2 <davidslovikosky@email.arizona.edu>
2018-08-27 12:27:31 -0700

    Refactoring text drawing

--- src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/standardControls/AOTDGuiLabel.java
+++ src/main/java/com/DavidM1A2/AfraidOfTheDark/client/gui/standardControls/AOTDGuiLabel.java
@@ -1,17 +1,30 @@
 package com.DavidM1A2.afraidofthedark.client.gui.standardControls;
 
-import com.DavidM1A2.afraidofthedark.client.gui.base.AOTDGuiTextContainer;
+import com.DavidM1A2.afraidofthedark.client.gui.base.AOTDGuiContainer;
+import com.DavidM1A2.afraidofthedark.client.gui.base.TextAlignment;
 import com.DavidM1A2.afraidofthedark.client.gui.fontLibrary.TrueTypeFont;
+import org.apache.commons.lang3.StringUtils;
+import org.lwjgl.util.Color;
 
 /**
  * Class representing a label to be drawn on the GUI
  */
-public class AOTDGuiLabel extends AOTDGuiTextContainer
+public class AOTDGuiLabel extends AOTDGuiContainer
 {
 	// The maximum string length
 	private int maxStrLength = Integer.MAX_VALUE;
-	// The string to be drawn
-	private String actualStringToDraw = "";
+	// The raw text we are supposed to draw
+	private String originalText = "";
+	// The text to draw
+	private String text = StringUtils.EMPTY;
+	// The font to draw the text with
+	private TrueTypeFont font;
+	// The color to draw the text with
+	private Color textColor = new Color(255, 255, 255, 255);
+	// Text alignment
+	private TextAlignment textAlignment = TextAlignment.ALIGN_LEFT;
+	// We draw at a lower scale because otherwise we get blurry text
+	private float drawingScale = 0.25f;
 
 	/**
 	 * Constructor takes an x and y position as well as a font
@@ -22,8 +35,9 @@ public class AOTDGuiLabel extends AOTDGuiTextContainer
 	 */
 	public AOTDGuiLabel(int x, int y, TrueTypeFont font)
 	{
-		super(x, y, 0, 0, font);
-		this.setHeight(Math.toIntExact(Math.round(font.getHeight() * this.getDrawingScale())));
+		super(x, y, 0, 0);
+		this.font = font;
+		this.setHeight(Math.toIntExact(Math.round(font.getHeight() * this.drawingScale)));
 	}
 
 	/**
@@ -36,35 +50,46 @@ public class AOTDGuiLabel extends AOTDGuiTextContainer
 		if (this.isVisible())
 		{
 			super.draw();
-			this.drawText(this.getXScaled(), this.getYScaled());
+
+			// If we don't have a font, we can't draw anything
+			if (this.font != null)
+			{
+				// Draw the string at (x, y) with the correct color and scale
+				this.font.drawString(
+						(float) this.getXScaled(),
+						(float) this.getYScaled(),
+						this.text,
+						this.getScaleX().floatValue() * drawingScale,
+						this.getScaleY().floatValue() * drawingScale,
+						textAlignment,
+						this.textColor.getRed() / 255f, this.textColor.getGreen() / 255f, this.textColor.getBlue() / 255f, this.textColor.getAlpha() / 255f);
+			}
 		}
 	}
 
 	/**
 	 * @param text Sets the text to be drawn
 	 */
-	@Override
 	public void setText(String text)
 	{
 		// Set the internal text of the label
-		super.setText(text);
+		this.originalText = text;
 		// If the internal text is too long, set the actual string to draw's length to be shorter
-		if (super.getText().length() > maxStrLength)
+		if (this.originalText.length() > maxStrLength)
 			// Shorten the internal string to a shorter representation
-			this.actualStringToDraw = super.getText().substring(0, maxStrLength);
+			this.text = this.originalText.substring(0, maxStrLength);
 		else
 			// Just use the internal representation as the text
-			this.actualStringToDraw = super.getText();
-		this.setWidth(Math.toIntExact(Math.round(this.getFont().getWidth(this.actualStringToDraw) * this.getDrawingScale())));
+			this.text = this.originalText;
+		this.setWidth(Math.toIntExact(Math.round(this.font.getWidth(this.text) * this.drawingScale)));
 	}
 
 	/**
 	 * @return Gets the text that this label represents
 	 */
-	@Override
 	public String getText()
 	{
-		return this.actualStringToDraw;
+		return this.originalText;
 	}
 
 	/**
@@ -72,15 +97,83 @@ public class AOTDGuiLabel extends AOTDGuiTextContainer
 	 *
 	 * @param maxStrLength The max length of the string
 	 */
+	/*
 	public void setMaxStringLength(int maxStrLength)
 	{
 		// Update the max string length
 		this.maxStrLength = maxStrLength;
-		// Update the actual string to draw
-		if (super.getText().length() > this.maxStrLength)
-			this.actualStringToDraw = super.getText().substring(0, this.maxStrLength);
+		// If the internal text is too long, set the actual string to draw's length to be shorter
+		if (this.originalText.length() > maxStrLength)
+			// Shorten the internal string to a shorter representation
+			this.textRenderer.setText(this.originalText.substring(0, maxStrLength));
 		else
-			this.actualStringToDraw = super.getText();
-		this.setWidth(Math.toIntExact(Math.round(this.getFont().getWidth(this.actualStringToDraw) * this.getDrawingScale())));
+			// Just use the internal representation as the text
+			this.textRenderer.setText(this.originalText);
+		this.setWidth(Math.toIntExact(Math.round(this.textRenderer.getFont().getWidth(this.textRenderer.getText()) * this.textRenderer.getDrawingScale())));
+	}
+	*/
+
+	/**
+	 * @param font Sets the font to be used while drawing the text
+	 */
+	public void setFont(TrueTypeFont font)
+	{
+		this.font = font;
+	}
+
+	/**
+	 * @return Gets the font to be used while drawing the text
+	 */
+	public TrueTypeFont getFont()
+	{
+		return this.font;
+	}
+
+	/**
+	 * @param textColor Sets the text's color
+	 */
+	public void setTextColor(Color textColor)
+	{
+		this.textColor = textColor;
+	}
+
+	/**
+	 * @return Gets the text's color
+	 */
+	public Color getTextColor()
+	{
+		return this.textColor;
+	}
+
+	/**
+	 * @param textAlignment Text alignment used by the label
+	 */
+	public void setTextAlignment(TextAlignment textAlignment)
+	{
+		this.textAlignment = textAlignment;
+	}
+
+	/**
+	 * @return Get the text alignment used by the label
+	 */
+	public TextAlignment getTextAlignment()
+	{
+		return this.textAlignment;
+	}
+
+	/**
+	 * @param drawingScale Sets the scale to draw the text at
+	 */
+	public void setDrawingScale(float drawingScale)
+	{
+		this.drawingScale = drawingScale;
+	}
+
+	/**
+	 * @return Gets the text drawing scale
+	 */
+	public float getDrawingScale()
+	{
+		return this.drawingScale;
 	}
 }
