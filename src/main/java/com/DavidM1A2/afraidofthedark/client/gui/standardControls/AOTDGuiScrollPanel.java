beb2e1e79d1a93925655d11cf8d0117fdcb98320
David Slovikosky <davidslovikosky@gmail.com>
2019-06-04 13:18:44 -0700

    Fixed GLScissor issues

--- src/main/java/com/DavidM1A2/afraidofthedark/client/gui/standardControls/AOTDGuiScrollPanel.java
+++ src/main/java/com/DavidM1A2/afraidofthedark/client/gui/standardControls/AOTDGuiScrollPanel.java
@@ -2,7 +2,11 @@ package com.DavidM1A2.afraidofthedark.client.gui.standardControls;
 
 import com.DavidM1A2.afraidofthedark.client.gui.AOTDGuiUtility;
 import com.DavidM1A2.afraidofthedark.client.gui.base.AOTDGuiContainer;
+import net.minecraft.util.math.MathHelper;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+
+import java.nio.IntBuffer;
 
 /**
  * Extension of a standard panel that requires a scroll bar to move content up and down
@@ -56,6 +60,30 @@ public class AOTDGuiScrollPanel extends AOTDGuiContainer
             // Compute the OpenGL width and height to scissor with
             int realWidth = guiUtility.mcToRealScreenCoord(this.getWidthScaled());
             int realHeight = guiUtility.mcToRealScreenCoord(this.getHeightScaled());
+
+            // If open GL scissors is enabled update the x,y width,height to be clamped within the current scissor box
+            if (GL11.glIsEnabled(GL11.GL_SCISSOR_TEST))
+            {
+                // Create an int buffer to hold all the current scissor box values
+                IntBuffer buffer = BufferUtils.createIntBuffer(16);
+                // Grab the current scissor box values
+                GL11.glGetInteger(GL11.GL_SCISSOR_BOX, buffer);
+                // Grab the old scissor rect values from the buffer
+                int oldX = buffer.get();
+                int oldY = buffer.get();
+                int oldWidth = buffer.get();
+                int oldHeight = buffer.get();
+                // Clamp the new scissor values within the old scissor box
+                realX = MathHelper.clamp(realX, oldX, oldX + oldWidth);
+                realY = MathHelper.clamp(realY, oldY, oldY + oldHeight);
+                realWidth = MathHelper.clamp(realWidth, 0, oldX + oldWidth - realX);
+                realHeight = MathHelper.clamp(realHeight, 0, oldY + oldHeight - realY);
+                // Don't draw anything if we're completely ouside the original box
+                if (realWidth == 0 || realHeight == 0)
+                {
+                    return;
+                }
+            }
 
             // Push the current scissor bit and enable scissor
             GL11.glPushAttrib(GL11.GL_SCISSOR_BIT);
