1c9b474dbc7e11ddcb718c1da12bb7624d4d0be3
David Slovikosky <davidslovikosky@gmail.com>
2019-12-06 00:00:56 -0800

    Updated potions to Kotlin

---
+++ src/main/java/com/davidm1a2/afraidofthedark/common/potion/PotionSleeping.kt
@@ -0,0 +1,11 @@
+package com.davidm1a2.afraidofthedark.common.potion
+
+import com.davidm1a2.afraidofthedark.common.potion.base.AOTDPotion
+import java.awt.Color
+
+/**
+ * Sleeping potion makes players 'drowsy'
+ *
+ * @@C   just sets item properties
+ */
+class PotionSleeping : AOTDPotion("Drowsiness", "sleeping_potion", 0, 0, false, Color(255, 255, 255))
---
+++ src/main/java/com/davidm1a2/afraidofthedark/common/potion/base/AOTDPotion.kt
@@ -0,0 +1,60 @@
+package com.davidm1a2.afraidofthedark.common.potion.base
+
+import com.davidm1a2.afraidofthedark.common.constants.Constants
+import net.minecraft.client.Minecraft
+import net.minecraft.potion.Potion
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.awt.Color
+
+/**
+ * Base class for all AOTD potions
+ *
+ * @constructor sets item properties
+ * @param name         The name of the potion for the players to see
+ * @param registryName The name of the potion in the game registry
+ * @param indexX       The X index of the position on the texture sheet
+ * @param indexY       The Y index of the position on the texture sheet
+ * @param isBad        True if the potion is 'bad', false otherwise
+ * @param color        The color of the potion
+ */
+open class AOTDPotion(name: String, registryName: String, indexX: Int, indexY: Int, isBad: Boolean, color: Color) : Potion(isBad, color.hashCode())
+{
+    init
+    {
+        this.setRegistryName(Constants.MOD_ID + ":" + registryName)
+        setPotionName(name)
+        setIconIndex(indexX, indexY)
+    }
+
+    /**
+     * Whenever MC wants to render a potion effect inject our custom textures and then render it
+     *
+     * @return The index of our custom potion texture sheet
+     */
+    @SideOnly(Side.CLIENT)
+    override fun getStatusIconIndex(): Int
+    {
+        Minecraft.getMinecraft().textureManager.bindTexture(AOTD_POTION_TEXTURES)
+        return super.getStatusIconIndex()
+    }
+
+    /**
+     * True if the duration is greater than 0
+     *
+     * @param duration  The duration left on the drink
+     * @param amplifier The potion amplifier
+     * @return True if the potion is ready, false otherwise
+     */
+    override fun isReady(duration: Int, amplifier: Int): Boolean
+    {
+        return duration >= 1
+    }
+
+    companion object
+    {
+        // Due to the crappy MC design all potion textures are on one PNG, so here we load that sheet of textures in
+        private val AOTD_POTION_TEXTURES = ResourceLocation(Constants.MOD_ID, "textures/potion_effect/potion_effects.png")
+    }
+}
