29734bd397b945ee4501c0371afd7295db7f9520
David Slovikosky <davidslovikosky@gmail.com>
2020-03-09 21:40:15 -0700

    Temporarily disable desert oasis and reduce gnomish city chance

--- src/main/java/com/davidm1a2/afraidofthedark/common/constants/ModStructures.kt
+++ src/main/java/com/davidm1a2/afraidofthedark/common/constants/ModStructures.kt
@@ -12,7 +12,7 @@ object ModStructures {
     val DARK_FOREST = StructureDarkForest()
     val NIGHTMARE_ISLAND = StructureNightmareIsland()
     val GNOMISH_CITY = StructureGnomishCity()
-    val DESERT_OASIS = StructureDesertOasis()
+    // val DESERT_OASIS = StructureDesertOasis()
 
     var STRUCTURE_LIST = arrayOf(
         CRYPT,
@@ -20,7 +20,7 @@ object ModStructures {
         VOID_CHEST,
         DARK_FOREST,
         NIGHTMARE_ISLAND,
-        GNOMISH_CITY,
-        DESERT_OASIS
+        GNOMISH_CITY
+        // DESERT_OASIS
     )
 }
--- src/main/java/com/davidm1a2/afraidofthedark/common/worldGeneration/structure/StructureGnomishCity.kt
+++ src/main/java/com/davidm1a2/afraidofthedark/common/worldGeneration/structure/StructureGnomishCity.kt
@@ -54,8 +54,8 @@ class StructureGnomishCity : AOTDStructure("gnomish_city") {
             }
 
             override fun getResult(): Double {
-                // .1% chance to generate in any chunks this fits in
-                return 0.001 * AfraidOfTheDark.INSTANCE.configurationHandler.gnomishCityFrequency
+                // .05% chance to generate in any chunks this fits in
+                return 0.0005 * AfraidOfTheDark.INSTANCE.configurationHandler.gnomishCityFrequency
             }
 
             override fun getDefaultResult(): Double {
