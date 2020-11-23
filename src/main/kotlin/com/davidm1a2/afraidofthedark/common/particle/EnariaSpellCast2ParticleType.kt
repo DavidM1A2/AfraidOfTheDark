d47944b46c2380c74aeb0c214b7f2c243a49d0bc
David Slovikosky <davidslovikosky@gmail.com>
2020-11-22 18:57:56 -0700

    Fix particle registry name to patch json file

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/particle/EnariaSpellCast2ParticleType.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/particle/EnariaSpellCast2ParticleType.kt
@@ -1,3 +1,3 @@
 package com.davidm1a2.afraidofthedark.common.particle
 
-class EnariaSpellCast2ParticleType : AOTDParticleType("enaria_spell_cast2", false)
+class EnariaSpellCast2ParticleType : AOTDParticleType("enaria_spell_cast_2", false)
