0b3376587bd5acac86785a0daf9fdad228374d80
Caleb <cprigge14@gmail.com>
2021-05-05 18:58:09 -0700

    Add new spell power source "blood magic"

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModSpellPowerSources.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModSpellPowerSources.kt
@@ -3,6 +3,7 @@ package com.davidm1a2.afraidofthedark.common.constants
 import com.davidm1a2.afraidofthedark.common.spell.component.powerSource.CreativeSpellPowerSource
 import com.davidm1a2.afraidofthedark.common.spell.component.powerSource.ExperienceSpellPowerSource
 import com.davidm1a2.afraidofthedark.common.spell.component.powerSource.FlaskSpellPowerSource
+import com.davidm1a2.afraidofthedark.common.spell.component.powerSource.HealthSpellPowerSource
 
 /**
  * A static class containing all of our spell power source references for us
@@ -11,11 +12,13 @@ object ModSpellPowerSources {
     val CREATIVE = CreativeSpellPowerSource()
     val EXPERIENCE = ExperienceSpellPowerSource()
     val FLASK = FlaskSpellPowerSource()
+    val HEALTH = HealthSpellPowerSource()
 
     // An array containing a list of spell power sources that AOTD adds
     val SPELL_POWER_SOURCES = arrayOf(
         CREATIVE,
         EXPERIENCE,
-        FLASK
+        FLASK,
+        HEALTH
     )
 }
