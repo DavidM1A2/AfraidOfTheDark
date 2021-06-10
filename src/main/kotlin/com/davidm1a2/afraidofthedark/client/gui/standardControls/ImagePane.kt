d86fac2368967758b442aa903bab0c96959157c6
David Slovikosky <davidslovikosky@gmail.com>
2021-06-09 18:56:26 -0700

    Replace TextureDimensionsCache with OpenGL equivalent calls

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/gui/standardControls/ImagePane.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/gui/standardControls/ImagePane.kt
@@ -1,27 +1,32 @@
 package com.davidm1a2.afraidofthedark.client.gui.standardControls
 
-import com.davidm1a2.afraidofthedark.client.gui.TextureDimensionsCache
 import com.davidm1a2.afraidofthedark.client.gui.layout.RelativeDimensions
 import com.mojang.blaze3d.platform.GlStateManager
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.AbstractGui
 import net.minecraft.util.ResourceLocation
+import org.lwjgl.opengl.GL11
 import kotlin.math.roundToInt
 
 /**
  * Class representing an image to be rendered on the GUI
  */
 open class ImagePane(
-        var imageTexture: ResourceLocation,
-        var displayMode: DispMode = DispMode.STRETCH) :
-        AOTDPane() {
-
-    constructor(imageTexture: String, displayMode: DispMode = DispMode.STRETCH): this(ResourceLocation(imageTexture), displayMode)
-
+    var imageTexture: ResourceLocation,
+    var displayMode: DispMode = DispMode.STRETCH
+) : AOTDPane() {
     var u = 0.0f
     var v = 0.0f
-    var textureWidth = TextureDimensionsCache.cache.get(imageTexture).width
-    var textureHeight = TextureDimensionsCache.cache.get(imageTexture).height
+    var textureWidth: Double
+    var textureHeight: Double
+
+    init {
+        Minecraft.getInstance().textureManager.bindTexture(imageTexture)
+        textureWidth = GL11.glGetTexLevelParameteri(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_WIDTH).toDouble()
+        textureHeight = GL11.glGetTexLevelParameteri(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_HEIGHT).toDouble()
+    }
+
+    constructor(imageTexture: String, displayMode: DispMode = DispMode.STRETCH) : this(ResourceLocation(imageTexture), displayMode)
 
     /**
      * Draws the GUI image given the width and height
@@ -58,22 +63,22 @@ open class ImagePane(
         val fitHeight = if (height < calcPrefHeight) height else calcPrefHeight
         when (displayMode) {
             DispMode.FIT_TO_TEXTURE -> {
-                val scaleXRatio = (fitWidth/textureWidth).coerceAtMost(1.0)
-                val scaleYRatio = (fitHeight/textureHeight).coerceAtMost(1.0)
+                val scaleXRatio = (fitWidth / textureWidth).coerceAtMost(1.0)
+                val scaleYRatio = (fitHeight / textureHeight).coerceAtMost(1.0)
                 val scaleMinRatio = scaleXRatio.coerceAtMost(scaleYRatio)
                 this.width = (textureWidth * scaleMinRatio).roundToInt()
                 this.height = (textureHeight * scaleMinRatio).roundToInt()
             }
             DispMode.FIT_TO_PARENT -> {
-                val scaleXRatio = fitWidth/textureWidth
-                val scaleYRatio = fitHeight/textureHeight
+                val scaleXRatio = fitWidth / textureWidth
+                val scaleYRatio = fitHeight / textureHeight
                 val scaleMinRatio = scaleXRatio.coerceAtMost(scaleYRatio)
                 this.width = (textureWidth * scaleMinRatio).roundToInt()
                 this.height = (textureHeight * scaleMinRatio).roundToInt()
             }
             DispMode.STRETCH -> {
-                val scaleXRatio = fitWidth/textureWidth
-                val scaleYRatio = fitHeight/textureHeight
+                val scaleXRatio = fitWidth / textureWidth
+                val scaleYRatio = fitHeight / textureHeight
                 this.width = (textureWidth * scaleXRatio).roundToInt()
                 this.height = (textureHeight * scaleYRatio).roundToInt()
             }
