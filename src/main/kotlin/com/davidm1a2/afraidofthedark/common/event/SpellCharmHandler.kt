54a37d9cacaecd4215fcf376d61e423bb858b0df
David Slovikosky <davidslovikosky@gmail.com>
2021-06-20 15:57:58 -0700

    Fix charm effect look code to not modify position

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/event/SpellCharmHandler.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/event/SpellCharmHandler.kt
@@ -1,13 +1,12 @@
 package com.davidm1a2.afraidofthedark.common.event
 
 import com.davidm1a2.afraidofthedark.common.capabilities.getSpellCharmData
+import net.minecraft.command.arguments.EntityAnchorArgument
 import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraft.world.server.ServerWorld
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.LogicalSide
-import kotlin.math.asin
-import kotlin.math.atan2
 
 /**
  * Handles events that let the player be charmed
@@ -40,29 +39,8 @@ class SpellCharmHandler {
                     if (charmingEntity != null) {
                         // A player cant charm themselves
                         if (entityPlayer.uniqueID != charmingEntityId) {
-                            val playerEyePosition = entityPlayer.getEyePosition(1.0f)
-                            // Compute the vector from the charming entity to the charmed entity
-                            val direction = charmingEntity.getEyePosition(1.0f)
-                                .subtract(playerEyePosition.x, playerEyePosition.y, playerEyePosition.z)
-                                .normalize()
-
-                            // Convert 3d direction vector to pitch and yaw. Yes, coerceIn() is required. Due to java's float precision not being very high,
-                            // direction vectors might be slightly outside of the [-1, 1] bounds, eg: -1.0000000118067964
-                            val xDirection = direction.x.coerceIn(-1.0, 1.0)
-                            val zDirection = direction.z.coerceIn(-1.0, 1.0)
-                            val yDirection = direction.y.coerceIn(-1.0, 1.0)
-                            val yaw = (-atan2(xDirection, zDirection) * 180 / Math.PI).toFloat()
-                            val pitch = (-asin(yDirection) * 180 / Math.PI).toFloat()
-
                             // Set the player's look to be at the charming entity
-                            (entityPlayer as ServerPlayerEntity).connection.setPlayerLocation(
-                                // TODO: In forge 1.15, this should not be needed. Use 0s and make the TP relative
-                                entityPlayer.posX,
-                                entityPlayer.posY,
-                                entityPlayer.posZ,
-                                yaw,
-                                pitch
-                            )
+                            (entityPlayer as ServerPlayerEntity).lookAt(EntityAnchorArgument.Type.EYES, charmingEntity, EntityAnchorArgument.Type.EYES)
                         }
                     }
                 }
