82634a07f833ffcb73f8ef58326ad2d8620477cb
David Slovikosky <davidslovikosky@gmail.com>
2021-06-28 14:35:50 -0700

    Fix hiding dimension type fields when in singleplayer

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModDimensions.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModDimensions.kt
@@ -3,8 +3,6 @@ package com.davidm1a2.afraidofthedark.common.constants
 import com.davidm1a2.afraidofthedark.common.dimension.nightmare.NightmareModDimension
 import com.davidm1a2.afraidofthedark.common.dimension.voidChest.VoidChestModDimension
 import net.minecraft.world.dimension.DimensionType
-import net.minecraftforge.api.distmarker.Dist
-import net.minecraftforge.api.distmarker.OnlyIn
 
 /**
  * Mod dimensions class initializes dimension types used in AOTD
@@ -17,10 +15,7 @@ object ModDimensions {
     // fields when connecting to a dedicated server. Why? Because RegisterDimensionsEvent never fires client side. Why
     // store these at all then? It turns out teleporting a player requires use of this "DimensionType" object. Since we
     // only teleport players from server side, this is fine.
-    @OnlyIn(Dist.DEDICATED_SERVER)
     lateinit var VOID_CHEST_TYPE: DimensionType
-
-    @OnlyIn(Dist.DEDICATED_SERVER)
     lateinit var NIGHTMARE_TYPE: DimensionType
 
     val DIMENSION_LIST = arrayOf(
