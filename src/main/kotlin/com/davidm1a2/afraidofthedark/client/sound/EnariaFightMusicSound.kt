07c5c10abca23cb449ccc242b32bf2aebddc2efa
David Slovikosky <davidslovikosky@gmail.com>
2021-06-30 18:31:58 -0700

    Fix Enaria music stopping too early

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/sound/EnariaFightMusicSound.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/sound/EnariaFightMusicSound.kt
@@ -1,8 +1,6 @@
 package com.davidm1a2.afraidofthedark.client.sound
 
 import com.davidm1a2.afraidofthedark.common.constants.ModSounds
-import com.davidm1a2.afraidofthedark.common.entity.enaria.EnariaEntity
-import net.minecraft.client.Minecraft
 import net.minecraft.util.SoundCategory
 
 /**
@@ -11,24 +9,10 @@ import net.minecraft.util.SoundCategory
  * @constructor sets the sound event to play and sound category
  * @property enariaId The ID of the Enaria entity
  */
-class EnariaFightMusicSound(private val enariaId: Int) :
-    PlayerFollowingSound(ModSounds.ENARIA_FIGHT_MUSIC, SoundCategory.PLAYERS) {
+class EnariaFightMusicSound(private val enariaId: Int) : PlayerFollowingSound(ModSounds.ENARIA_FIGHT_MUSIC, SoundCategory.PLAYERS) {
     init {
         // This sound loops
         repeat = true
         repeatDelay = 0
-    }
-
-    /**
-     * Ensure that this only players in the nightmare when the player is not dead
-     */
-    override fun tick() {
-        super.tick()
-
-        val enaria = Minecraft.getInstance().player!!.world.getEntityByID(enariaId) as? EnariaEntity
-        // If the enaria entity doesn't exist kill the sound
-        if (enaria == null) {
-            donePlaying = true
-        }
     }
 }
