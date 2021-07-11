f5e0db153459f8598c05cc6df5faccf4494285c8
David Slovikosky <davidslovikosky@gmail.com>
2021-07-10 17:15:22 -0700

    Update client/dimension to 1.15.2

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/dimension/NightmareSkyRenderer.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/dimension/NightmareSkyRenderer.kt
@@ -1,25 +1,18 @@
 package com.davidm1a2.afraidofthedark.client.dimension
 
+import com.mojang.blaze3d.matrix.MatrixStack
 import net.minecraft.client.Minecraft
 import net.minecraft.client.world.ClientWorld
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
-import net.minecraftforge.client.IRenderHandler
+import net.minecraftforge.client.SkyRenderHandler
 
 /**
  * Class that renders the nightmare 'sky' texture
  */
-class NightmareSkyRenderer : IRenderHandler {
-    /**
-     * Called to render the sky
-     *
-     * @param ticks        The number of ticks that have happened
-     * @param partialTicks The number of partial ticks since the last tick
-     * @param world        The world to render in
-     * @param mc           The minecraft instance
-     */
+class NightmareSkyRenderer : SkyRenderHandler {
     @OnlyIn(Dist.CLIENT)
-    override fun render(ticks: Int, partialTicks: Float, world: ClientWorld, mc: Minecraft) {
+    override fun render(ticks: Int, partialTicks: Float, matrixStack: MatrixStack, world: ClientWorld, mc: Minecraft) {
         // Empty for now, might have a skybox later
     }
 }
--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/dimension/VoidChestSkyRenderer.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/dimension/VoidChestSkyRenderer.kt
@@ -1,84 +1,88 @@
 package com.davidm1a2.afraidofthedark.client.dimension
 
 import com.davidm1a2.afraidofthedark.common.constants.Constants
+import com.mojang.blaze3d.matrix.MatrixStack
 import com.mojang.blaze3d.platform.GlStateManager
+import com.mojang.blaze3d.systems.RenderSystem
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.RenderHelper
 import net.minecraft.client.renderer.Tessellator
+import net.minecraft.client.renderer.Vector3f
+import net.minecraft.client.renderer.WorldVertexBufferUploader
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.client.world.ClientWorld
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
-import net.minecraftforge.client.IRenderHandler
+import net.minecraftforge.client.SkyRenderHandler
 import org.lwjgl.opengl.GL11
 
 /**
  * Class that renders the void chest 'sky' texture
  */
-class VoidChestSkyRenderer : IRenderHandler {
-    /**
-     * Called to render the sky
-     *
-     * @param ticks        The number of ticks that have happened
-     * @param partialTicks The number of partial ticks since the last tick
-     * @param world        The world to render in
-     * @param mc           The minecraft instance
-     */
+class VoidChestSkyRenderer : SkyRenderHandler {
     @OnlyIn(Dist.CLIENT)
-    override fun render(ticks: Int, partialTicks: Float, world: ClientWorld, mc: Minecraft) {
+    override fun render(ticks: Int, partialTicks: Float, matrixStack: MatrixStack, world: ClientWorld, mc: Minecraft) {
         ///
-        /// Code below found online and modified slightly
+        /// Code below is similar to WorldRenderer::renderSky()
         ///
 
-        GlStateManager.disableFog()
-        GlStateManager.disableAlphaTest()
-        GlStateManager.enableBlend()
-        GlStateManager.blendFuncSeparate(770, 771, 1, 0)
+        RenderSystem.disableFog()
+        RenderSystem.disableAlphaTest()
+        RenderSystem.enableBlend()
+        RenderSystem.blendFuncSeparate(
+            GlStateManager.SourceFactor.SRC_ALPHA,
+            GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+            GlStateManager.SourceFactor.ONE,
+            GlStateManager.DestFactor.ZERO
+        )
         RenderHelper.disableStandardItemLighting()
-        GlStateManager.depthMask(false)
+        RenderSystem.depthMask(false)
         val tessellator = Tessellator.getInstance()
         val bufferBuilder = tessellator.buffer
 
         for (i in 0..5) {
-            GlStateManager.pushMatrix()
+            matrixStack.push()
 
             when (i) {
                 1 -> {
                     mc.textureManager.bindTexture(VOID_CHEST_SKY_SIDE_2)
-                    GlStateManager.rotatef(90.0f, 1.0f, 0.0f, 0.0f)
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(90.0f))
                 }
                 2 -> {
                     mc.textureManager.bindTexture(VOID_CHEST_SKY_SIDE_4)
-                    GlStateManager.rotatef(-90.0f, 1.0f, 0.0f, 0.0f)
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(-90.0f))
                 }
                 3 -> {
                     mc.textureManager.bindTexture(VOID_CHEST_SKY_TOP)
-                    GlStateManager.rotatef(180.0f, 1.0f, 0.0f, 0.0f)
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(180.0f))
                 }
                 4 -> {
                     mc.textureManager.bindTexture(VOID_CHEST_SKY_SIDE_3)
-                    GlStateManager.rotatef(90.0f, 0.0f, 0.0f, 1.0f)
+                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(90.0f))
                 }
                 5 -> {
                     mc.textureManager.bindTexture(VOID_CHEST_SKY_SIDE_1)
-                    GlStateManager.rotatef(-90.0f, 0.0f, 0.0f, 1.0f)
+                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(-90.0f))
                 }
                 else -> mc.textureManager.bindTexture(VOID_CHEST_SKY_BOTTOM)
             }
 
+            val matrix = matrixStack.last.matrix
             bufferBuilder.begin(7, DefaultVertexFormats.POSITION_TEX)
-            bufferBuilder.pos(-100.0, -100.0, -100.0).tex(0.0, 0.0).endVertex()
-            bufferBuilder.pos(-100.0, -100.0, 100.0).tex(0.0, 1.0).endVertex()
-            bufferBuilder.pos(100.0, -100.0, 100.0).tex(1.0, 1.0).endVertex()
-            bufferBuilder.pos(100.0, -100.0, -100.0).tex(1.0, 0.0).endVertex()
-            tessellator.draw()
-            GlStateManager.popMatrix()
+            bufferBuilder.pos(matrix, -100.0f, -100.0f, -100.0f).tex(0.0f, 0.0f).endVertex()
+            bufferBuilder.pos(matrix, -100.0f, -100.0f, 100.0f).tex(0.0f, 1.0f).endVertex()
+            bufferBuilder.pos(matrix, 100.0f, -100.0f, 100.0f).tex(1.0f, 1.0f).endVertex()
+            bufferBuilder.pos(matrix, 100.0f, -100.0f, -100.0f).tex(1.0f, 0.0f).endVertex()
+            bufferBuilder.finishDrawing()
+            WorldVertexBufferUploader.draw(bufferBuilder)
+
+            matrixStack.pop()
         }
 
-        GlStateManager.depthMask(true)
+        RenderSystem.depthMask(true)
         GL11.glEnable(GL11.GL_TEXTURE_2D)
-        GlStateManager.enableAlphaTest()
+        RenderSystem.enableAlphaTest()
     }
 
     companion object {
