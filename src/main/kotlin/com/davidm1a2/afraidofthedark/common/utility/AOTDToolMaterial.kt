fcfcb477a145c3e44040b5c90f1e8fbfbf6c40db
David Slovikosky <davidslovikosky@gmail.com>
2021-07-12 18:01:20 -0700

    Update common/utility to 1.15.2

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/utility/AOTDToolMaterial.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/utility/AOTDToolMaterial.kt
@@ -2,7 +2,6 @@ package com.davidm1a2.afraidofthedark.common.utility
 
 import net.minecraft.item.IItemTier
 import net.minecraft.item.crafting.Ingredient
-import net.minecraft.util.LazyLoadBase
 
 class AOTDToolMaterial(
     private val harvestLevel: Int,
@@ -10,14 +9,12 @@ class AOTDToolMaterial(
     private val efficiency: Float,
     private val attackDamage: Float,
     private val enchantability: Int,
-    repairMaterial: () -> Ingredient
+    private val repairMaterial: Ingredient
 ) : IItemTier {
-    private val repairMaterial = LazyLoadBase(repairMaterial)
-
     override fun getHarvestLevel() = harvestLevel
     override fun getMaxUses() = maxUses
     override fun getEfficiency() = efficiency
     override fun getAttackDamage() = attackDamage
     override fun getEnchantability() = enchantability
-    override fun getRepairMaterial(): Ingredient = repairMaterial.value
+    override fun getRepairMaterial() = repairMaterial
 }
