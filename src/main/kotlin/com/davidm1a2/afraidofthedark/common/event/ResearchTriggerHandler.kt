3fefec837dcd61da92a590b500ad7b8bd52d711f
David Slovikosky <davidslovikosky@gmail.com>
2021-08-29 13:49:42 -0700

    Cleanup ResearchTriggerHandler research mapping

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/event/ResearchTriggerHandler.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/event/ResearchTriggerHandler.kt
@@ -12,17 +12,15 @@ import kotlin.reflect.KClass
  * Contains logic for handling research unlocks.
  */
 class ResearchTriggerHandler {
-    private val hooks = mutableMapOf<KClass<out Event>, MutableList<ConfiguredResearchTrigger<in Event, *, *>>>()
-    private val triggerToResearch = mutableMapOf<ConfiguredResearchTrigger<*, *, *>, Research>()
+    private val hooks = mutableMapOf<KClass<out Event>, MutableList<Pair<ConfiguredResearchTrigger<in Event, *, *>, Research>>>()
 
     @SubscribeEvent
     fun onEvent(event: Event) {
-        hooks[event::class]?.forEach {
-            val player = it.getAffectedPlayer(event)
+        hooks[event::class]?.forEach { (trigger, research) ->
+            val player = trigger.getAffectedPlayer(event)
             // Only process hooks server side, otherwise we risk processing each event twice in singleplayer.
             if (player != null && !player.level.isClientSide) {
-                if (it.shouldUnlock(player, event)) {
-                    val research = triggerToResearch[it]!!
+                if (trigger.shouldUnlock(player, event)) {
                     val playerResearch = player.getResearch()
                     if (playerResearch.canResearch(research)) {
                         playerResearch.setResearch(research, true)
@@ -38,8 +36,7 @@ class ResearchTriggerHandler {
             research.triggers.forEach {
                 val hookList = hooks.computeIfAbsent(it.trigger.type) { mutableListOf() }
                 @Suppress("UNCHECKED_CAST")
-                hookList.add(it as ConfiguredResearchTrigger<in Event, *, *>)
-                triggerToResearch[it] = research
+                hookList.add(it as ConfiguredResearchTrigger<in Event, *, *> to research)
             }
         }
     }
