555b899535719b6eb864257528d0c97ab9049c6b
David Slovikosky <davidslovikosky@gmail.com>
2021-10-10 18:36:42 -0700

    Setup Bone Sword research

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModToolMaterials.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/constants/ModToolMaterials.kt
@@ -7,7 +7,7 @@ import net.minecraft.item.crafting.Ingredient
  * Class storing tool material constants. These are registered through the enum helper class and not through the standard forge system
  */
 object ModToolMaterials {
-    val BONE = AOTDToolMaterial(0, 150, 0f, 0f, 0, Ingredient.EMPTY)
+    val BONE = AOTDToolMaterial(0, 150, 0f, 0f, 0, Ingredient.of(ModItems.ENCHANTED_SKELETON_BONE))
     val ASTRAL_SILVER = AOTDToolMaterial(2, 250, 1f, 3.0f, 20, Ingredient.of(ModItems.ASTRAL_SILVER_INGOT))
     val IGNEOUS = AOTDToolMaterial(3, 600, 1f, 8f, 15, Ingredient.EMPTY)
     val STAR_METAL = AOTDToolMaterial(3, 600, 1f, 7f, 15, Ingredient.EMPTY)
