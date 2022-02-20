786ebfbcaf90959e2079f6cb0c6af07e27d9e72c
David Slovikosky <davidslovikosky@gmail.com>
2022-02-20 12:19:07 -0700

    Fix spell damage localization

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/utility/damagesource/SpellDamageSource.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/utility/damagesource/SpellDamageSource.kt
@@ -7,15 +7,15 @@ import net.minecraft.util.text.ITextComponent
 import net.minecraft.util.text.TranslationTextComponent
 
 class SpellDamageSource(private val spellDamageState: DeliveryTransitionState) : EntityDamageSource("afraidofthedark.spell", spellDamageState.casterEntity) {
-    override fun getLocalizedDeathMessage(attackingEntity: LivingEntity): ITextComponent {
+    override fun getLocalizedDeathMessage(killedEntity: LivingEntity): ITextComponent {
         val baseText = "death.attack.$msgId"
         val spellCaster = spellDamageState.casterEntity
-        return if (attackingEntity == spellCaster) {
-            TranslationTextComponent("$baseText.suicide", attackingEntity.displayName)
+        return if (killedEntity == spellCaster) {
+            TranslationTextComponent("$baseText.suicide", killedEntity.displayName)
         } else if (spellCaster != null) {
-            TranslationTextComponent("$baseText.player", entity?.displayName, spellCaster.displayName, spellDamageState.spell.name)
+            TranslationTextComponent("$baseText.player", killedEntity.displayName, spellCaster.displayName, spellDamageState.spell.name)
         } else {
-            TranslationTextComponent(baseText, entity?.displayName, entity?.displayName)
+            TranslationTextComponent(baseText, killedEntity.displayName, spellDamageState.spell.name)
         }
     }
 }
