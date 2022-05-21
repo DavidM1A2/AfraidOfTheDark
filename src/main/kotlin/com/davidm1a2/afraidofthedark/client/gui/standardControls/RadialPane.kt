e7cdcefaf55ed6372e1e2094dc4c39ed7e9457ac
Caleb <cprigge@email.arizona.edu>
2022-05-20 17:41:13 -0700

    Implement RadialPane

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/gui/standardControls/RadialPane.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/gui/standardControls/RadialPane.kt
@@ -1,7 +1,11 @@
 package com.davidm1a2.afraidofthedark.client.gui.standardControls
 
 import com.davidm1a2.afraidofthedark.client.gui.layout.Gravity
+import com.davidm1a2.afraidofthedark.client.gui.layout.Position
+import kotlin.math.PI
+import kotlin.math.cos
 import kotlin.math.roundToInt
+import kotlin.math.sin
 
 /**
  * Lays out child GUI elements in a radial manner, interpreting (x,y) offsets as (r,theta) coordinates.
@@ -23,22 +27,23 @@ open class RadialPane : RatioPane(1, 1) {
             // Calculate position
             val gravityXOffset = when (child.gravity) {
                 Gravity.TOP_LEFT, Gravity.CENTER_LEFT, Gravity.BOTTOM_LEFT -> calcMargins.left
-                Gravity.TOP_CENTER, Gravity.CENTER, Gravity.BOTTOM_CENTER -> internalWidth / 2 - (child.width + calcMargins.width) / 2 + calcMargins.left
-                Gravity.TOP_RIGHT, Gravity.CENTER_RIGHT, Gravity.BOTTOM_RIGHT -> internalWidth - child.width - calcMargins.right
+                Gravity.TOP_CENTER, Gravity.CENTER, Gravity.BOTTOM_CENTER -> -(child.width + calcMargins.width) / 2 + calcMargins.left
+                Gravity.TOP_RIGHT, Gravity.CENTER_RIGHT, Gravity.BOTTOM_RIGHT -> -child.width - calcMargins.right
             }
             val gravityYOffset = when (child.gravity) {
                 Gravity.TOP_LEFT, Gravity.TOP_CENTER, Gravity.TOP_RIGHT -> calcMargins.top
-                Gravity.CENTER_LEFT, Gravity.CENTER, Gravity.CENTER_RIGHT -> internalHeight / 2 - (child.height + calcMargins.height) / 2 + calcMargins.top
-                Gravity.BOTTOM_LEFT, Gravity.BOTTOM_CENTER, Gravity.BOTTOM_RIGHT -> internalHeight - child.height - calcMargins.bot
+                Gravity.CENTER_LEFT, Gravity.CENTER, Gravity.CENTER_RIGHT -> -(child.height + calcMargins.height) / 2 + calcMargins.top
+                Gravity.BOTTOM_LEFT, Gravity.BOTTOM_CENTER, Gravity.BOTTOM_RIGHT -> -child.height - calcMargins.bot
             }
-            // TODO: Treat child offset as polar coordinates
-            val offset = child.offset.getAbsolute(this)
+            // Treat children offsets as polar coordinates (r,theta)
+            val rVal = if (child.offset.isRelative) child.offset.x*internalWidth/2 else child.offset.x
+            val tVal = if (child.offset.isRelative) child.offset.y*2*PI else child.offset.y
             // Set position
-            child.x = (this.x + this.guiOffsetX + calcPadding.left + gravityXOffset + offset.x).roundToInt()
-            child.y = (this.y + this.guiOffsetY + calcPadding.top + gravityYOffset + offset.y).roundToInt()
+            child.x = (this.x + this.guiOffsetX + calcPadding.left + gravityXOffset + sin(tVal)*rVal).roundToInt()
+            child.y = (this.y + this.guiOffsetY + calcPadding.top + gravityYOffset + cos(tVal)*rVal).roundToInt()
             // If it's a pane, have it recalculate its children too
             if (child is AOTDPane) child.calcChildrenBounds()
-            // Determine if the subtree of children are in this node's bounds
+            // Determine if the subtree of children is in this node's bounds
             determineInBounds(child)
         }
     }
