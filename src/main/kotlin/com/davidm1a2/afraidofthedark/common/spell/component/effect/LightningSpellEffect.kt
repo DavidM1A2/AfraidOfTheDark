7a12df3a4f8b55f824cdfda17d066d8f287deb73
Caleb <cprigge@email.arizona.edu>
2022-05-31 15:09:53 -0700

    Make lightning effect cheaper to facilitate aoe use

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/LightningSpellEffect.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/LightningSpellEffect.kt
@@ -21,6 +21,6 @@ class LightningSpellEffect : AOTDSpellEffect("lightning", ModResearches.INSANITY
     }
 
     override fun getCost(instance: SpellComponentInstance<SpellEffect>): Double {
-        return 10.0
+        return 7.0
     }
 }
