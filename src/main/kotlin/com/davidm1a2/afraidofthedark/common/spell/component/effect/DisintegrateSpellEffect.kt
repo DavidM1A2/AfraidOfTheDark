19206e74cba0161bd433fa5d72cd3a7574bd78ac
David Slovikosky <davidslovikosky@gmail.com>
2022-08-25 22:55:41 -0700

    Stop disintegrate from working on bedrock

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/DisintegrateSpellEffect.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/DisintegrateSpellEffect.kt
@@ -9,6 +9,8 @@ import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.AOTDSpel
 import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.SpellEffect
 import com.davidm1a2.afraidofthedark.common.spell.component.property.SpellComponentPropertyFactory
 import net.minecraft.block.Blocks
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class DisintegrateSpellEffect : AOTDSpellEffect("disintegrate", ModResearches.MAGIC_MASTERY) {
     init {
@@ -32,13 +34,25 @@ class DisintegrateSpellEffect : AOTDSpellEffect("disintegrate", ModResearches.MA
         createParticlesAt(2, 6, exactPosition, world.dimension(), ModParticles.DISINTEGRATE)
         if (entityHit != null) {
             entityHit.hurt(ModDamageSources.getSpellDamage(state), strength)
-        } else {
+        } else if (canBlockBeDestroyed(world, state.blockPosition)) {
             world.setBlockAndUpdate(state.blockPosition, Blocks.AIR.defaultBlockState())
         }
     }
 
     override fun getCost(instance: SpellComponentInstance<SpellEffect>): Double {
         return getStrength(instance) * 2.0 - 1
+    }
+
+    /**
+     * Tests if a given block can be broken with a disintegrate spell
+     *
+     * @param world    The world the block is in
+     * @param blockPos The pos the block is at
+     * @return True if the block can be destroyed, false otherwise
+     */
+    private fun canBlockBeDestroyed(world: World, blockPos: BlockPos): Boolean {
+        val blockState = world.getBlockState(blockPos)
+        return blockState.getDestroySpeed(world, blockPos) != -1f
     }
 
     fun setStrength(instance: SpellComponentInstance<*>, amount: Float) {
