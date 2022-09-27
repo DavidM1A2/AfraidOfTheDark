404992511fd7b0f57f7fd9ab83b3ab4997a08cb1
David Slovikosky <davidslovikosky@gmail.com>
2022-09-27 00:20:51 -0700

    Update explosion particle

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/particle/ExplosionParticle.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/particle/ExplosionParticle.kt
@@ -1,10 +1,15 @@
 package com.davidm1a2.afraidofthedark.client.particle
 
+import com.davidm1a2.afraidofthedark.common.spell.component.effect.ExplosionSpellEffect
 import net.minecraft.client.particle.IAnimatedSprite
 import net.minecraft.client.particle.IParticleFactory
 import net.minecraft.client.particle.Particle
 import net.minecraft.client.world.ClientWorld
 import net.minecraft.particles.BasicParticleType
+import net.minecraft.util.math.MathHelper
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /**
  * Particle representing a player's spell cast
@@ -19,15 +24,34 @@ class ExplosionParticle(
     world: ClientWorld,
     x: Double,
     y: Double,
-    z: Double
+    z: Double,
+    explosionRadius: Double
 ) : AOTDParticle(world, x, y, z) {
+    private val fadeSpeed = FADE_SPEED_MIN + random.nextDouble() * (FADE_SPEED_MAX - FADE_SPEED_MIN)
+    private val sinOffset = random.nextDouble() * 2 * PI
+
     init {
-        // 1 second lifespan
-        lifetime = 20
+        scale(random.nextFloat() * 3f + 1f)
+
+        // 3-8 second lifespan based on radius
+        lifetime = MathHelper.lerp(explosionRadius / ExplosionSpellEffect.MAX_RADIUS, 60.0, 160.0).toInt()
+
         // Random motion
-        xd = (random.nextFloat() - 0.5) * 0.5
-        yd = (random.nextFloat() - 0.5) * 0.5
-        zd = (random.nextFloat() - 0.5) * 0.5
+        xd = (random.nextFloat() - 0.5) * 0.8 * sqrt(explosionRadius)
+        yd = (random.nextFloat() - 0.1) * 0.8 * sqrt(explosionRadius)
+        zd = (random.nextFloat() - 0.5) * 0.8 * sqrt(explosionRadius)
+    }
+
+    override fun updateMotionXYZ() {
+        xd = xd * 0.95
+        yd = (yd - 0.08).coerceAtLeast(-2.0)
+        zd = zd * 0.95
+
+        val oscillation = (sin(sinOffset + age * fadeSpeed).toFloat() + 1f) / 2f
+        val fadeAlpha = oscillation * 0.5f + 0.5f
+        setAlphaFadeInLastTicks(40f)
+        alpha = alpha * fadeAlpha
+        setColor(fadeAlpha, fadeAlpha, fadeAlpha)
     }
 
     class Factory(private val spriteSet: IAnimatedSprite) : IParticleFactory<BasicParticleType> {
@@ -37,13 +61,19 @@ class ExplosionParticle(
             x: Double,
             y: Double,
             z: Double,
+            // Technically 'xSpeed', but actually used as explosion radius
             xSpeed: Double,
             ySpeed: Double,
             zSpeed: Double
         ): Particle {
-            return ExplosionParticle(world, x, y, z).apply {
+            return ExplosionParticle(world, x, y, z, xSpeed).apply {
                 pickSprite(spriteSet)
             }
         }
+    }
+
+    companion object {
+        private const val FADE_SPEED_MIN = 0.24
+        private const val FADE_SPEED_MAX = 0.32
     }
 }
--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/ExplosionSpellEffect.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/ExplosionSpellEffect.kt
@@ -2,12 +2,15 @@ package com.davidm1a2.afraidofthedark.common.spell.component.effect
 
 import com.davidm1a2.afraidofthedark.common.constants.ModParticles
 import com.davidm1a2.afraidofthedark.common.constants.ModResearches
+import com.davidm1a2.afraidofthedark.common.network.packets.other.ParticlePacket
 import com.davidm1a2.afraidofthedark.common.spell.component.DeliveryTransitionState
 import com.davidm1a2.afraidofthedark.common.spell.component.SpellComponentInstance
 import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.AOTDSpellEffect
 import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.SpellEffect
 import com.davidm1a2.afraidofthedark.common.spell.component.property.SpellComponentPropertyFactory
+import net.minecraft.util.math.vector.Vector3d
 import net.minecraft.world.Explosion
+import kotlin.math.floor
 
 /**
  * Effect that creates an explosion at the given position
@@ -23,7 +26,7 @@ class ExplosionSpellEffect : AOTDSpellEffect("explosion", ModResearches.MAGIC_MA
                 .withGetter(this::getRadius)
                 .withDefaultValue(2.0f)
                 .withMinValue(1.0f)
-                .withMaxValue(40.0f)
+                .withMaxValue(MAX_RADIUS)
                 .build()
         )
     }
@@ -37,7 +40,16 @@ class ExplosionSpellEffect : AOTDSpellEffect("explosion", ModResearches.MAGIC_MA
     override fun procEffect(state: DeliveryTransitionState, instance: SpellComponentInstance<SpellEffect>) {
         val world = state.world
         val position = state.position
-        createParticlesAt(5, 10, position, world.dimension(), ModParticles.EXPLOSION)
+        val numParticles = floor(getRadius(instance) * getRadius(instance)).toInt().coerceAtLeast(10)
+        createParticlesAt(
+            state, ParticlePacket.builder()
+                .particle(ModParticles.EXPLOSION)
+                .position(position)
+                // Particle's 'speed' isn't actually speed. It's the explosion's radius. We'll re-use the speed's x coordinate though since it's unused
+                .speed(Vector3d(getRadius(instance).toDouble(), 0.0, 0.0))
+                .iterations(numParticles)
+                .build()
+        )
         world.explode(null, position.x, position.y - 0.01f, position.z, getRadius(instance), Explosion.Mode.BREAK)
     }
 
@@ -67,5 +79,7 @@ class ExplosionSpellEffect : AOTDSpellEffect("explosion", ModResearches.MAGIC_MA
     companion object {
         // NBT constants
         private const val NBT_RADIUS = "radius"
+
+        const val MAX_RADIUS = 40.0f
     }
 }
