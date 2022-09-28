4d64d78eaf39959981a722754673138e7cd88357
David Slovikosky <davidslovikosky@gmail.com>
2022-09-27 21:59:19 -0700

    Update grow particles

--- src/main/kotlin/com/davidm1a2/afraidofthedark/client/particle/GrowParticle.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/client/particle/GrowParticle.kt
@@ -5,6 +5,7 @@ import net.minecraft.client.particle.IParticleFactory
 import net.minecraft.client.particle.Particle
 import net.minecraft.client.world.ClientWorld
 import net.minecraft.particles.BasicParticleType
+import kotlin.random.Random
 
 /**
  * Particle representing a player's spell cast
@@ -22,18 +23,30 @@ class GrowParticle(
     z: Double
 ) : AOTDParticle(world, x, y, z) {
     init {
+        scale(0.3f + Random.nextFloat())
+
         // 1-1.5 second lifespan
         lifetime = 20 + random.nextInt(10)
         // Drift Upwards
         xd = (random.nextDouble() - 0.5) * 0.3
         yd = random.nextDouble() * 0.3
         zd = (random.nextDouble() - 0.5) * 0.3
+
+        rCol = 0.5f
+        gCol = 0.5f
+        bCol = 0.5f
     }
 
     override fun updateMotionXYZ() {
         xd *= 0.9
         yd *= 0.95
         zd *= 0.9
+        setAlphaFadeInLastTicks(3f)
+        if (age <= 6) {
+            rCol = age / 6f
+            gCol = age / 6f
+            bCol = age / 6f
+        }
     }
 
     class Factory(private val spriteSet: IAnimatedSprite) : IParticleFactory<BasicParticleType> {
--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/GrowSpellEffect.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/spell/component/effect/GrowSpellEffect.kt
@@ -2,13 +2,16 @@ package com.davidm1a2.afraidofthedark.common.spell.component.effect
 
 import com.davidm1a2.afraidofthedark.common.constants.ModParticles
 import com.davidm1a2.afraidofthedark.common.constants.ModResearches
+import com.davidm1a2.afraidofthedark.common.network.packets.other.ParticlePacket
 import com.davidm1a2.afraidofthedark.common.spell.component.DeliveryTransitionState
 import com.davidm1a2.afraidofthedark.common.spell.component.SpellComponentInstance
 import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.AOTDSpellEffect
 import com.davidm1a2.afraidofthedark.common.spell.component.effect.base.SpellEffect
 import com.davidm1a2.afraidofthedark.common.spell.component.property.SpellComponentPropertyFactory
 import net.minecraft.block.IGrowable
+import net.minecraft.util.math.vector.Vector3d
 import net.minecraftforge.common.util.Constants
+import kotlin.random.Random
 
 /**
  * Spell effect that causes growable blocks to grow
@@ -35,7 +38,7 @@ class GrowSpellEffect : AOTDSpellEffect("grow", ModResearches.APPRENTICE_ASCENDE
      */
     override fun procEffect(state: DeliveryTransitionState, instance: SpellComponentInstance<SpellEffect>) {
         val world = state.world
-        var position = state.blockPosition
+        var position = state.entity?.blockPosition() ?: state.blockPosition
         var blockState = world.getBlockState(position)
 
         // If we hit a block that crops might be on check the block above and below and to if we can grow on that instead
@@ -52,11 +55,13 @@ class GrowSpellEffect : AOTDSpellEffect("grow", ModResearches.APPRENTICE_ASCENDE
         // Grob the block at the current position if it's a type 'IGrowable'
         for (ignored in 0 until getStrength(instance)) {
             if (blockState.block !is IGrowable) {
+                createFizzleParticleAt(state)
                 break
             }
 
             val iGrowable = blockState.block as IGrowable
             if (!iGrowable.isValidBonemealTarget(world, position, blockState, false)) {
+                createFizzleParticleAt(state)
                 break
             }
 
@@ -64,7 +69,6 @@ class GrowSpellEffect : AOTDSpellEffect("grow", ModResearches.APPRENTICE_ASCENDE
                 continue
             }
 
-            createParticlesAround(8, 16, state.position, world.dimension(), ModParticles.GROW, 2.0)
             iGrowable.performBonemeal(world, world.random, position, blockState)
             blockUpdated = true
             blockState = world.getBlockState(position)
@@ -73,6 +77,14 @@ class GrowSpellEffect : AOTDSpellEffect("grow", ModResearches.APPRENTICE_ASCENDE
         if (blockUpdated) {
             world.sendBlockUpdated(position, blockState, blockState, Constants.BlockFlags.BLOCK_UPDATE)
         }
+        createParticlesAt(
+            state, ParticlePacket.builder()
+                .particle(ModParticles.GROW)
+                .positions(List(if (blockUpdated) 5 else 2) {
+                    Vector3d(position.x + Random.nextDouble(), position.y + Random.nextDouble(), position.z + Random.nextDouble())
+                })
+                .build()
+        )
     }
 
     /**
