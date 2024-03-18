e70df29bd9581738da3610cf4991dfbf92566b2d
David Slovikosky <davidslovikosky@gmail.com>
2024-03-17 19:43:04 -0700

    Create new tags for blocks with HarvestLevel and HarvestTool

--- src/main/kotlin/com/davidm1a2/afraidofthedark/common/block/AmorphousEldritchMetalBlock.kt
+++ src/main/kotlin/com/davidm1a2/afraidofthedark/common/block/AmorphousEldritchMetalBlock.kt
@@ -3,14 +3,13 @@ package com.davidm1a2.afraidofthedark.common.block
 import com.davidm1a2.afraidofthedark.common.block.core.AOTDBlock
 import com.davidm1a2.afraidofthedark.common.constants.ModResearches
 import com.davidm1a2.afraidofthedark.common.event.custom.ManualResearchTriggerEvent
-import net.minecraft.block.BlockState
-import net.minecraft.block.material.Material
-import net.minecraft.entity.Entity
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
+import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.Material
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.common.ToolType
 
 /**
  * Class representing amorphous eldritch metal, this can be walked through
@@ -22,14 +21,12 @@ class AmorphousEldritchMetalBlock : AOTDBlock(
     Properties.of(Material.PORTAL)
         .noCollission()
         .lightLevel { 1 }
-        .harvestLevel(2)
-        .harvestTool(ToolType.PICKAXE)
         .strength(10.0f, 50.0f)
         .requiresCorrectToolForDrops()
 ) {
-    override fun entityInside(blockState: BlockState, world: World, blockPos: BlockPos, entity: Entity) {
+    override fun entityInside(blockState: BlockState, world: Level, blockPos: BlockPos, entity: Entity) {
         super.entityInside(blockState, world, blockPos, entity)
-        if (!world.isClientSide && entity is PlayerEntity) {
+        if (!world.isClientSide && entity is Player) {
             MinecraftForge.EVENT_BUS.post(ManualResearchTriggerEvent(entity, ModResearches.SNOWY_ANOMALY))
         }
     }
